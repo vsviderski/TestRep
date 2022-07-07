9908d6420a23eb88c28980b407e26163963e5c0b
V.Sviderski <svidvlad15@gmail.com>
2022-07-07 11:11:35 +0300

    added translation

--- NewEmployee.tsx
+++ NewEmployee.tsx
@@ -12,6 +12,7 @@ import CalendarInput from './CalendarInput';
 import SelectUnique from './SelectUnique';
 
 const NewEmployee: FC = (): JSX.Element => {
+  const [t] = useTranslation();
 
   const {
     onRequestsContainerClick,
