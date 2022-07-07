d795809e94b87034e61e931116630139eac703b0
V.Sviderski <svidvlad15@gmail.com>
2022-07-07 10:06:18 +0300

    changed auth

--- auth.js
+++ auth.js
@@ -1,6 +1,7 @@
 function auth(params) {
     if (params.id) {
-        console.log('Successful authentication')
+        console.log('Successful authentication');
+        console.log('something else');
     } else {
         console.log('something went wrong');
     }
