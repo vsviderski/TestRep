9128c324d6dfd3cf0b2237d8338ed5da5d3c9d12
V.Sviderski <svidvlad15@gmail.com>
2022-07-07 10:00:58 +0300

    First commit

---
+++ NewEmployee.tsx
@@ -0,0 +1,185 @@
+import { FC, useEffect } from 'react';
+import { useTranslation } from 'react-i18next';
+import RequestsContainer from 'Components/Requests/RequestsContainer';
+import LocationList from 'Components/Requests/Controls/LocationList';
+import NotifiersList from 'Components/Requests/Controls/NotifiersList';
+import { useNewEmployee } from './useNewEmployee';
+import Select from './Select';
+import Input from './Input';
+import UnitsSelect from './UnitsSelect';
+import TextAreaComponent from './TextArea';
+import CalendarInput from './CalendarInput';
+import SelectUnique from './SelectUnique';
+
+const NewEmployee: FC = (): JSX.Element => {
+  const [t] = useTranslation();
+
+  const {
+    onSubmit,
+    onRequestsContainerClick,
+    setDataForSelects,
+    getNotifiers,
+    notifiersListRef,
+    units,
+    workplacesItems,
+    onChangeLocation,
+    setEmptyWorkplace,
+    organizations,
+    ranks,
+    positions,
+    registration,
+    wageRates,
+    contractDurations,
+    contractTypes,
+    employeeEquipment,
+    technologicalProfiles,
+    curators,
+    booleansSelectItems,
+    reasons,
+    reasonRef,
+  } = useNewEmployee();
+
+  useEffect(() => {
+    setDataForSelects();
+  }, []);
+
+  return (
+    <RequestsContainer
+      text={t('requests.newEmployee.newEmployee')}
+      onSubmit={onSubmit}
+      onClick={onRequestsContainerClick}
+    >
+      <SelectUnique
+        label={t('requestsContainer.location')}
+        child={<LocationList ref={onChangeLocation} />}
+      />
+
+      <Select
+        label={t('requests.newEmployee.reason')}
+        refProp={reasonRef}
+        elements={reasons}
+        isError={false}
+      />
+
+      <Select
+        label={t('requests.newEmployee.organization')}
+        elements={organizations}
+        isError={false}
+      />
+
+      <Input label={t('requests.newEmployee.secondNameEn')} />
+      <Input label={t('requests.newEmployee.firstNameEn')} />
+      <Input label={t('requests.newEmployee.secondNameLocal')} />
+      <Input label={t('requests.newEmployee.firstNameLocal')} />
+      <Input label={t('requests.newEmployee.middleName')} />
+      <Input label={t('requests.newEmployee.phone')} />
+      <Input label={t('requests.newEmployee.identification')} />
+
+      <UnitsSelect units={units} />
+
+      <Select
+        label={t('requests.newEmployee.workplace')}
+        elements={workplacesItems}
+        isError={false}
+        refProp={setEmptyWorkplace}
+      />
+
+      <Select
+        label={t('requests.newEmployee.rank')}
+        elements={ranks}
+        isError={false}
+        isSearch
+      />
+
+      <Select
+        label={t('requests.newEmployee.position')}
+        elements={positions}
+        isError={false}
+        isSearch
+      />
+
+      <Input label={t('requests.newEmployee.salaryGross')} />
+
+      <CalendarInput
+        label={t('requests.newEmployee.employmentDate')}
+        id="date-id"
+      />
+
+      <Select
+        label={t('requests.newEmployee.student')}
+        elements={booleansSelectItems}
+        isError={false}
+      />
+
+      <Select
+        label={t('requests.newEmployee.contract')}
+        elements={contractTypes}
+        isError={false}
+      />
+
+      <Select
+        label={t('requests.newEmployee.contractDuration')}
+        elements={contractDurations}
+        isError={false}
+      />
+
+      <Select
+        label={t('requests.newEmployee.wageRate')}
+        elements={wageRates}
+        isError={false}
+      />
+
+      <Select
+        label={t('requests.newEmployee.trialPeriod')}
+        elements={booleansSelectItems}
+        isError={false}
+      />
+
+      <Select
+        label={t('requests.newEmployee.registration')}
+        elements={registration}
+        isError={false}
+      />
+
+      <Input label={t('requests.newEmployee.currentAddress')} isOptional />
+
+      <Select
+        label={t('requests.newEmployee.equipment')}
+        elements={employeeEquipment}
+        isError={false}
+      />
+
+      <TextAreaComponent
+        label={t('requests.newEmployee.additionalEquipment')}
+        id="additionalEquipment-id"
+      />
+
+      <Select
+        label={t('requests.newEmployee.technologicalProfile')}
+        elements={technologicalProfiles}
+        isError={false}
+      />
+
+      <Select
+        label={t('requests.newEmployee.curator')}
+        elements={curators}
+        isError={false}
+        isSearch
+      />
+
+      <TextAreaComponent
+        label={t('requests.newEmployee.details')}
+        id="details-id"
+      />
+
+      <SelectUnique
+        label={t('requestsContainer.notificationRecipients')}
+        child={
+          <NotifiersList getNotifiers={getNotifiers} ref={notifiersListRef} />
+        }
+      />
+    </RequestsContainer>
+  );
+};
+
+export default NewEmployee;
---
+++ auth.js
@@ -0,0 +1,7 @@
+function auth(params) {
+    if (params.id) {
+        console.log('Successful authentication')
+    } else {
+        console.log('something went wrong');
+    }
+};
